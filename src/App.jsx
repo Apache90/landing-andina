550bae5a8b1ce5ec0d47946bf0de7cf5a34ca9c0
Emir Segovia <95300946+Apache90@users.noreply.github.com>
2023-09-29 21:10:57 -0300

    se agrego boton de wsap y se modifico leyenda de footer a SoftFusion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer/Footer";
+import WhatsappBut from "./components/WhatsappBut";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero />
+      <WhatsappBut/>
       <Footer />
     </div>
     
